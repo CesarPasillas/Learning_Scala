33b8e018af101c23b3965f7d9ee53a1be9932870
CesarPasillas <42359065+CesarPasillas@users.noreply.github.com>
2023-05-11 23:50:18 -0500

    Create HelloWorld.scala
    adding the file Hello World

---
+++ src/main/scala/com/programmingsquirrel/HelloWorld.scala
@@ -0,0 +1,16 @@
+package com.programmingsquirrel
+
+/**
+ * This simple Scala Hello World application.
+ * You can try Scala and play around with any Scala code you like!
+ *
+ * (Of course that you can create your own applications, this is only to know how is a Scala application.)
+ *
+ * This code prints only our Hello World
+ *
+ * @Autor Cesar Pasillas
+ */
+object HelloWorld extends App {
+
+  println("Hell World");
+}
