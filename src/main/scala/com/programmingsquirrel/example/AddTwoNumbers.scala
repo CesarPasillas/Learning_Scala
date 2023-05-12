d91e906d4664c79c3ab17584773fc22f8be17f59
CesarPasillas <42359065+CesarPasillas@users.noreply.github.com>
2023-05-11 23:51:55 -0500

    Update AddTwoNumbers.scala

--- src/main/scala/com/programmingsquirrel/example/AddTwoNumbers.scala
+++ src/main/scala/com/programmingsquirrel/example/AddTwoNumbers.scala
@@ -1,1 +1,28 @@
+package com.programmingsquirrel.example
 
+/**
+ * Objeto that add two numbers and show the result in the console.
+ *
+ * (This application or code is to show how is an Scala code).
+ *
+ * @Autor Cesar Pasillas
+ */
+object AddTwoNumbers {
+
+  /**
+   * Principal method that is executed when the application Starts.
+   * @param args CLI Arguments (not are used on this example).
+   */
+  def main(args: Array[String]): Unit = {
+
+    // Define two numbers to add
+    val num1 = 10
+    val num2 = 5
+
+    // add the two numbers and stored into a new variable called sum
+    val sum = num1 + num2
+
+    // print the result in the console
+    println(s"The sum of $num1 and $num2 is $sum")
+  }
+}
